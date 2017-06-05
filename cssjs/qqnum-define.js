1fb066ae3dc0de5bf47cd5bb4686b98f800be4f1
bdhc_lp <luping_bj@sina.com>
2017-06-05 14:50:22 +0800

    aaa

---
+++ cssjs/qqnum-define.js
@@ -0,0 +1,9 @@
+$(function(){
+ 	$("body").Sonline({
+  		Position:"right",//left或right
+  		Top:200,//顶部距离，默认200px
+  		Effect:true, //滚动或者固定两种方式，布尔值：true或false
+  		DefaultsOpen:true, //默认展开：true,默认收缩：false
+  		Qqlist:"1878415734|客服一部,1261394400|客服二部,1878415734|客服三部,1430168223|客服四部" //多个QQ用','隔开，QQ和客服名用'|'隔开
+    });
+});
